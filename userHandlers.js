3644dda9af037ede63ba67ecc19ac8c70862913e
FITSCH YANN <Fitsch-yann@hotmail.fr>
2022-11-30 22:14:14 +0100

    express03 + 04

--- userHandlers.js
+++ userHandlers.js
@@ -1,5 +1,44 @@
 const database = require("./database");
 
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the users");
+    });
+};
+
 const getUsers = (req, res) => {
     database
       .query("select * from users")
@@ -33,5 +72,7 @@ const getUsers = (req, res) => {
   module.exports = {
     getUsers,
     getUsersId,
+    updateUser,
+    postUser,
   };
   
