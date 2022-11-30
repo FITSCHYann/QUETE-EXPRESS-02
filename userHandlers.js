52493aceca58baf749b8c2c6e5537af81b2917e2
FITSCH YANN <Fitsch-yann@hotmail.fr>
2022-11-30 22:29:25 +0100

    express 05

--- userHandlers.js
+++ userHandlers.js
@@ -1,5 +1,23 @@
 const database = require("./database");
 
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user");
+    });
+};
+
 const updateUser = (req, res) => {
   const id = parseInt(req.params.id);
   const { firstname, lastname, email, city, language } = req.body;
@@ -74,5 +92,6 @@ const getUsers = (req, res) => {
     getUsersId,
     updateUser,
     postUser,
+    deleteUser,
   };
   
